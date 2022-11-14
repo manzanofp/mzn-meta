f080b537cf820d35bdedbba3d43df6a2c3060456
manzanofp <felipemanzanofp@gmail.com>
2022-11-14 11:19:19 -0300

    project created

---
+++ backend-mznmeta/src/main/java/com/manzanofp/mznmeta/MznmetaApplication.java
@@ -0,0 +1,13 @@
+package com.manzanofp.mznmeta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MznmetaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MznmetaApplication.class, args);
+	}
+
+}
---
+++ backend-mznmeta/src/test/java/com/manzanofp/mznmeta/MznmetaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.manzanofp.mznmeta;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MznmetaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
