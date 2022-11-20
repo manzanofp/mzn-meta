2041638de6fa5d10902304468e1e2c40c9ba4e6b
manzanofp <felipemanzanofp@gmail.com>
2022-11-19 23:08:05 -0300

    Date Select

--- backend-mznmeta/src/main/java/com/manzanofp/mznmeta/controller/SaleController.java
+++ backend-mznmeta/src/main/java/com/manzanofp/mznmeta/controller/SaleController.java
@@ -3,11 +3,14 @@ package com.manzanofp.mznmeta.controller;
 import com.manzanofp.mznmeta.model.Sale;
 import com.manzanofp.mznmeta.service.SaleService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.List;
+
 
 @RestController
 @RequestMapping(value = "/sales")
@@ -16,10 +19,12 @@ public class SaleController {
     private SaleService saleService;
 
     @GetMapping
-    public List<Sale>findSales(){
-        return saleService.findSales();
+    public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
+                                @RequestParam(value = "minDate", defaultValue = "")String maxDate,
+                                Pageable pageable){
+        return saleService.findSales(minDate, maxDate, pageable);
     }
 
-    
+
 
 }
--- backend-mznmeta/src/main/java/com/manzanofp/mznmeta/service/SaleService.java
+++ backend-mznmeta/src/main/java/com/manzanofp/mznmeta/service/SaleService.java
@@ -2,19 +2,26 @@ package com.manzanofp.mznmeta.service;
 
 import com.manzanofp.mznmeta.model.Sale;
 import com.manzanofp.mznmeta.repository.SaleRepository;
+import net.bytebuddy.asm.Advice;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.time.ZoneId;
 
 @Service
 public class SaleService {
-
     @Autowired
     private SaleRepository saleRepository;
 
-    public List<Sale> findSales(){
-        return saleRepository.findAll();
+    public Page<Sale> findSales(String minDate, String maxDate, Pageable pageble){
+        LocalDate today = LocalDate.ofInstant(Instant.now(), ZoneId.systemDefault());
+        LocalDate min = minDate.equals("") ? today.minusDays(365) : LocalDate.parse(minDate);
+        LocalDate max = maxDate.equals("") ? today : LocalDate.parse(maxDate);
+        return saleRepository.findSales(min, max, pageble);
     }
 
 }
