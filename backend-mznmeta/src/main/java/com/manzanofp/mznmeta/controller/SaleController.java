893a7f59ed2ee792c1db87e659907e5731a348b4
manzanofp <felipemanzanofp@gmail.com>
2022-11-19 23:42:07 -0300

    Twilio

--- backend-mznmeta/src/main/java/com/manzanofp/mznmeta/controller/SaleController.java
+++ backend-mznmeta/src/main/java/com/manzanofp/mznmeta/controller/SaleController.java
@@ -2,14 +2,11 @@ package com.manzanofp.mznmeta.controller;
 
 import com.manzanofp.mznmeta.model.Sale;
 import com.manzanofp.mznmeta.service.SaleService;
+import com.manzanofp.mznmeta.service.SmsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.*;
 
 
 @RestController
@@ -18,6 +15,9 @@ public class SaleController {
     @Autowired
     private SaleService saleService;
 
+    @Autowired
+    private SmsService smsService;
+
     @GetMapping
     public Page<Sale> findSales(@RequestParam(value = "minDate", defaultValue = "") String minDate,
                                 @RequestParam(value = "minDate", defaultValue = "")String maxDate,
@@ -25,6 +25,10 @@ public class SaleController {
         return saleService.findSales(minDate, maxDate, pageable);
     }
 
+    @GetMapping("/{id}/notification")
+    public void notifySms(@PathVariable Long id){
+    smsService.sendSms(id);
+    }
 
 
 }
